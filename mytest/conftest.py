de8a2ae2a8f365ed9a7623b58917a556d65a5d4e
shamilanazIT-QA <shamilanaz9@gmail.com>
2026-04-29 11:55:31 -0500

    conftest.py updated

--- mytest/conftest.py
+++ mytest/conftest.py
@@ -5,18 +5,17 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
-@pytest.fixture
+@@pytest.fixture
 def driver():
     options = Options()
-    options.binary_location = r"C:\Users\shamila\AppData\Local\chrome\chrome.exe"
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("start-maximized")
-    service = Service(r"C:\Users\shamila\AppData\Local\chromedriver\chromedriver.exe")
-    driver = webdriver.Chrome(service=service, options=options)
+
+    # On GitHub Actions (Linux), Chrome is already installed.
+    # You don't need the service path or binary_location!
+    driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit()
-
 
 
