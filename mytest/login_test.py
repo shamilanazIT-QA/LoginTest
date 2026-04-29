03648f64af87773b98c8a2edfee7a9bca2e0e7dd
shamilanazIT-QA <shamilanaz9@gmail.com>
2026-04-29 12:09:31 -0500

    conftest.py updated once again

--- mytest/login_test.py
+++ mytest/login_test.py
@@ -2,17 +2,17 @@
 from pages.Login_Page import LoginPage  # Make sure this import is correct
 
 
-def test_valid_login(driver):  # 'driver' here comes from your fixture in conftest.py
+def test_valid_login(driver):
     login_pg = LoginPage(driver)
     driver.get("https://the-internet.herokuapp.com/login")
 
-    # Corrected usage
+    # Use the base 'type' method directly or update pass_keys to accept strings
     login_pg.type(login_pg.user_name, "tomsmith")
     login_pg.type(login_pg.password, "SuperSecretPassword!")
     login_pg.click_login()
 
-    # Add an assertion to see if it worked!
-    assert "secure" in driver.current_url
+    # This part is crucial!
+    assert "secure" in driver.page_source
 
 
 
